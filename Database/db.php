f3dd3709eae55358f41ecf5696ee3eb9f77e09c2
Jason Lin <157414523@qq.com>
2018-10-23 22:53:42 +0800

    delete the fake password

--- Database/db.php
+++ Database/db.php
@@ -2,7 +2,7 @@
 
 $dbServername = "127.0.0.1";
 $dbUsername = "root";
-$dbPassword = "wsadghjvbn";
+$dbPassword = "";
 $dbName = "unitytest";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
